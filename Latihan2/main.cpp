8f6b370698052f18a965f5079ca9eb6536159647
Zaenal Mustofa <zaenalmusthofa97@gmail.com>
2018-11-06 21:16:05 +0700

    Menentukan nilai ganjil dan genap suatu bilangan

---
+++ Latihan2/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int x;
+cout<<"Masukkan Bilangan :"<<endl;
+cin>>x;
+if (x%2==0)
+cout<<"Bilangan yang anda masukkan adalah Genap";
+else
+cout<<"Bilangan yang anda masukkan adalah Ganjil";
+
+return 0;
+}
